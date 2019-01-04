fc6fb53dc61600d35f823b7b2c0157781b57ee79
marta.bertran <martabertrangarcia@gmail.com>
2019-01-04 10:42:02 +0100

    Update 17_demo_upload_files.js

--- 17_demo_upload_files.js
+++ 17_demo_upload_files.js
@@ -5,23 +5,31 @@
 //STEP 2: PARSE THE UPLOADED FILE
 //STEP 3: SAVE THE FILE
 
-//Include the http module
+//(step 1) Include the http module 
 var http = require('http');
-//The Formidable module is good for working with file uploads. This module can be downloaded and installed using NPM.
-//The Formidable module is able to parse the uploaded file once it reaches the server.
+//(step 2)The Formidable module is good for working with file uploads. This module can be downloaded and installed using NPM.
+//The Formidable module is able to parse the uploaded file once it reaches the server. 
 var formidable = require('formidable');
+//(step 3) Include the fs module
+var fs = require('fs');
 
-//This code will produce an HTML form (step 1)
+//(step 1) This code will produce an HTML form 
 http.createServer(function(req, res) {
 
-  if(){
-  //This code will parse the uploaded file (step 2)
-  if(req.url == '/fileupload'){
+  //(step 2) This code will parse the uploaded file 
+  if(req.url == '/fileupload'){ 
+      //(step 2) When the file is uploaded and parsed, it gets placed on a temporary folder of your computer.
       var form = new formidable.IncomingForm();
       form.parse(req, function (err, fields, files) {
-        res.write('File uploaded');
-        res.end();
-      });
+        //(step 3) Move the file from the temporary folder to the current folder.
+        var oldpath = files.filetoupload.path;
+        var newpath = 'C:/Users/Your Name/' + files.filetoupload.name;
+        fs.rename(oldpath, newpath, function(err){
+          if (err) throw err;
+          res.write('File uploaded and moved!');
+          res.end();
+        });  
+  });
       
   } else {
     //This code will produce an HTML form (step 1)
